1c1d1d8488cdecc43f801ecd61152f7764b4cf62
coderold <mm35molina@gmail.com>
2024-10-07 22:37:44 +0800

    First commit
    set up lang ng project for future codes

---
+++ StudentInfoApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StudentNamespace
+{
+    public class StudenInfo
+    {
+
+    }
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            Console.ReadKey();
+        }
+    }
+}
